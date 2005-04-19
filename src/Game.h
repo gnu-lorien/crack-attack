97549b9f6d43702552baa91735fda33ac12d1ff9
Kevin Webb <kwebb@cc.gatech.edu>
2005-04-19 21:00:18 +0000

    Updated version number to 1.1.13-cvs.

--- src/Game.h
+++ src/Game.h
@@ -59,7 +59,7 @@ using namespace std;
 // the name
 #define GC_NAME                          "Crack Attack!"
 #define GC_BINARY                        "crack-attack"
-#define GC_VERSION                       "1.1.12"
+#define GC_VERSION                       "1.1.13-cvs"
 
 // startup message
 #define GC_MESSAGE                       GC_NAME " v" GC_VERSION "\n"
