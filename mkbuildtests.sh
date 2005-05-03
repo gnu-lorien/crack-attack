b364cfd96782fe02f7b6bf8990ead68931e0fcf7
Andrew Sayman <lorien420@myrealbox.com>
2005-05-03 04:54:19 +0000

    fixed problem with using -1 and --solo. These flags can't be used together.

--- mkbuildtests.sh
+++ mkbuildtests.sh
@@ -2,11 +2,11 @@
 
 MAKE='make clean && make && make install'
 echo "Running autogen.sh"
-./autogen.sh --prefix=`pwd`/inst > /dev/null
+./autogen.sh --prefix=`pwd`/inst &> /dev/null
 # Default build test
 echo "Testing default build"
 (./configure --prefix=`pwd`/inst \
-&& make clean && make && make install) > /dev/null
+&& make clean && make && make install) &> /dev/null
 echo "Game should start with GUI"
 ./inst/bin/crack-attack > /dev/null
 echo "Ensuring relocatable..."
@@ -19,18 +19,18 @@ mv ./moved_inst ./inst
 
 echo "Testing no gui"
 (./configure --prefix=`pwd`/inst --disable-gtk \
-&& make clean && make && make install) > /dev/null
+&& make clean && make && make install) &> /dev/null
 echo "Game should fail to start"
 ./inst/bin/crack-attack > /dev/null
 echo "Game should start with solo low extreme game"
-./inst/bin/crack-attack -1 -X --solo > /dev/null
+./inst/bin/crack-attack -1 -X > /dev/null
 
 echo "Testing no relocatable"
 (./configure --prefix=`pwd`/inst --disable-binreloc \
-&& make clean && make && make install) > /dev/null
+&& make clean && make && make install) &> /dev/null
 echo "Game should start with solo low extreme game"
-./inst/bin/crack-attack -1 -X --solo > /dev/null
+./inst/bin/crack-attack -1 -X > /dev/null
 mv ./inst ./moved_inst
 echo "Game should fail to start"
-./inst/bin/crack-attack -1 -X --solo > /dev/null
+./inst/bin/crack-attack -1 -X > /dev/null
 mv ./moved_inst ./inst
