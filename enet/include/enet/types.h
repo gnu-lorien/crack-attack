77140d68f6546964fb513c567debcea52f8796a6
Kevin Webb <kwebb@cc.gatech.edu>
2006-09-29 00:34:01 +0000

    Fixes for datatype size on amd64.

--- enet/include/enet/types.h
+++ enet/include/enet/types.h
@@ -5,9 +5,16 @@
 #ifndef __ENET_TYPES_H__
 #define __ENET_TYPES_H__
 
-typedef unsigned char enet_uint8;       /**< unsigned 8-bit type  */
-typedef unsigned short enet_uint16;     /**< unsigned 16-bit type */
-typedef unsigned int enet_uint32;      /**< unsigned 32-bit type */
+#ifdef WIN32
+  typedef unsigned char enet_uint8;       /**< unsigned 8-bit type  */
+  typedef unsigned short enet_uint16;     /**< unsigned 16-bit type */
+  typedef unsigned int enet_uint32;      /**< unsigned 32-bit type */
+#else
+  #include <inttypes.h>
+  typedef uint8_t enet_uint8;       /**< unsigned 8-bit type  */
+  typedef uint16_t enet_uint16;     /**< unsigned 16-bit type */
+  typedef uint32_t enet_uint32;      /**< unsigned 32-bit type */
+#endif
 
 #endif /* __ENET_TYPES_H__ */
 
