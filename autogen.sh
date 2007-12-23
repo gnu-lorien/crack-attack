130e560e04494c0e68579f20f0e27a861d752f08
Andrew Sayman <lorien420@myrealbox.com>
2007-12-23 04:55:35 -0500

    Documentation and build generation for Fedora Core 8

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-aclocal-1.8 $ACLOCAL_FLAGS || exit;
-autoheader-2.59 || exit;
-autoconf-2.59 || exit;
-automake-1.8 --add-missing --copy || exit;
+aclocal-1.10 $ACLOCAL_FLAGS || exit;
+autoheader || exit;
+autoconf || exit;
+automake-1.10 --add-missing --copy || exit;
 ./configure $@
 
